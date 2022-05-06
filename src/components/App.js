8d547bdc6a4a62a0b6acb24b1e3f421dcd866fad
Meridith <meridith.junk@gmail.com>
2022-05-06 10:26:13 -0500

    set up initial api call / tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>Hello World!</header>
-    </div>
-  );
+
+import React, {useState, useEffect} from "react";
+
+export default function App() {
+    const [note, setNote] = useState(null);
+
+    async function fetchUserData() {
+        let response = await fetch("http://localhost:5000/notes/1");
+        return response.json()
+    }
+
+    useEffect(() => {
+        fetchUserData().then((response) => {
+            setNote(response);
+        });
+    });
+
+    if (!note) {
+        return "loading...";
+    }
+
+    return (
+        <div>
+            <h1>{note.title}</h1>
+            <textarea  value={this.state.value} onChange={this.handleChange} >
+
+            </textarea>
+        </div>
+    )
 }
 
-export default App;
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import {render, screen, waitFor} from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+    const fakeNote = {
+        title: "Notes Title"
+    };
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+        json: jest.fn().mockResolvedValue(fakeNote)
+    });
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('Note Tests', () => {
+    it("Should render loading before note is loaded", async () => {
+        render(<App/>)
+        expect(screen.getByText(`loading...`)).toBeInTheDocument()
+    });
+
+    it("Should render note contents once note is loaded", async () => {
+        const { getByText } = render(<App/>)
+
+        await waitFor(() => {
+            expect(screen.getByText(`Notes Title`)).toBeInTheDocument()
+        });
+    });
 });
