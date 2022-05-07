59b39390c6a53b879054677ec96b26e23294fc6c
Meridith <meridith.junk@gmail.com>
2022-05-07 09:25:00 -0500

    update init

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Note Tests', () => {
+  it("Should render Hello World!", async () => {
+    render(<App/>)
+    expect(screen.getByText(`Hello World!`)).toBeInTheDocument()
+  });
 });
